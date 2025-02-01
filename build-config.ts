aa015e4ed0425c6d59cb831618b46b54c870a6bc
javadbat <javadbat@gmail.com>
2025-02-01 15:37:40 +0330

    feat: add build config

---
+++ build-config.ts
@@ -0,0 +1,25 @@
+import type { ReactComponentBuildConfig, WebComponentBuildConfig } from "../../tasks/build/builder/src/types.ts";
+
+export const webComponentList: WebComponentBuildConfig[] = [
+  {
+    name: "jb-calendar",
+    path: "./lib/jb-calendar.ts",
+    outputPath: "./dist/jb-calendar.js",
+    external: ["date-fns", "date-fns-jalali"],
+    umdName: "JBCalendar",
+    //because date-fns dont have any umd module export i have to do this so it doenst exclude in umd build
+    umdIncludes: ["date-fns", "date-fns-jalali"],
+  },
+];
+export const reactComponentList: ReactComponentBuildConfig[] = [
+  {
+    name: "jb-calendar-react",
+    path: "/web-component/jb-calendar/react/lib/JBCalendar.tsx",
+    outputPath: "/web-component/jb-calendar/react/dist/JBCalendar.js",
+    external: ["jb-calendar", "prop-types", "react"],
+    globals: {
+      react: "React",
+      "prop-types": "PropTypes",
+    },
+  },
+];
